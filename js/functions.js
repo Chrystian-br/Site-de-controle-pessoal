caede35887a09e3a0c9e06bdda87706577901fab
Chrystian Coelho <chrystiancoelho3212@outlook.com>
2021-09-17 14:04:15 -0300

    Entrada de informações completa

--- js/functions.js
+++ js/functions.js
@@ -34,15 +34,57 @@ $(function(){
     })
     /*****/
 
-    // Recebendo as informações do formulário e colocando-as no histórico
+    // Recebendo as informações do formulário, colocando-as no histórico e salvando-as no localStorage
+    if(localStorage.listaSalva == undefined || localStorage.listaSalva == 'NaN'){
+        var lista = [];
+        localStorage.listaSalva = lista;
+    } else {
+        lista = JSON.parse(localStorage.getItem('listaSalva'));
+    }
+
     function item(valor,desc,tipo){
         this.valor = valor;
         this.desc = desc;
         this.tipo = tipo;
     }
 
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i].tipo == 'Crédito'){
+            $('tbody').prepend(`<tr style="background-color: green">
+                <td>R$${lista[i].valor}</td>
+                <td>${lista[i].tipo}</td>
+                <td>${lista[i].desc}</td>
+            </tr>`)
+        } else if (lista[i].tipo == 'Despesa variável'){
+            $('tbody').prepend(`<tr style="background-color: red">
+                <td>R$${lista[i].valor}</td>
+                <td>${lista[i].tipo}</td>
+                <td>${lista[i].desc}</td>
+            </tr>`)
+        } else if (lista[i].tipo == 'Despesa fixa'){
+            $('tbody').prepend(`<tr style="background-color: orange">
+                <td>R$${lista[i].valor}</td>
+                <td>${lista[i].tipo}</td>
+                <td>${lista[i].desc}</td>
+            </tr>`)
+        } else if (lista[i].tipo == 'Investido'){
+            $('tbody').prepend(`<tr style="background-color: blue">
+                <td>R$${lista[i].valor}</td>
+                <td>${lista[i].tipo}</td>
+                <td>${lista[i].desc}</td>
+            </tr>`)
+        }
+    }
+
     $('input[type=button]').click(function(){
         var addItem = new item($('#entrada-valor').val(), $('#entrada-descricao').val(), $('#entrada-tipo').val())
+
+        lista.push(addItem);
+
+        var listaSalva = JSON.stringify(lista);
+        localStorage.listaSalva = listaSalva;
+
+        
 
         if(addItem.tipo == 'Crédito'){
             $('tbody').prepend(`<tr style="background-color: green">
