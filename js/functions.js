d3e4dba96b39c48f420a0d170245067ca2102579
Chrystian Coelho <chrystiancoelho3212@outlook.com>
2021-09-16 22:13:00 -0300

    Estilizando e dinamizando a página de entrada

--- js/functions.js
+++ js/functions.js
@@ -7,62 +7,67 @@ $(function(){
                 'width': '0%',
             }).fadeOut(5);
 
+            $('main > *').animate({
+                'left': '5%',
+                'width': '90%'
+            })
+
             $('.btn-menu').animate({
                 'left':'2.5%'
             })
 
-            $('main > *').animate({
-                'left':'50%'
-            })
         } else if($('.sidebar').css('width') == '0px') {
             $('.sidebar').fadeIn(10).animate({
                 'width':'20%'
+            })
+
+            $('main > *').animate({
+                'left': '20%',
+                'width':'82.5%'
             })
 
             $('.btn-menu').animate({
                 'left':'23%'
             })
 
-            $('main > *').animate({
-                'left':'60%'
-            })
         }
     })
     /*****/
 
-    // Recebendo as informações do formulário e inserindo-as no localStorage
+    // Recebendo as informações do formulário e colocando-as no histórico
+    function item(valor,desc,tipo){
+        this.valor = valor;
+        this.desc = desc;
+        this.tipo = tipo;
+    }
+
     $('input[type=button]').click(function(){
-        let valor = Number($('#entrada-valor').val());
-        let descricao = $('#entrada-descricao').val();
-        let tipo = $('#entrada-tipo').val();
+        var addItem = new item($('#entrada-valor').val(), $('#entrada-descricao').val(), $('#entrada-tipo').val())
 
-        if(tipo == 'investido'){
-            $('.investimento p').html(valorInvestido(valor))
-        } else {
-            $('.lucro p').html(valorLucrado(valor,tipo))
+        if(addItem.tipo == 'Crédito'){
+            $('tbody').prepend(`<tr style="background-color: green">
+                <td>R$${addItem.valor}</td>
+                <td>${addItem.tipo}</td>
+                <td>${addItem.desc}</td>
+            </tr>`)
+        } else if (addItem.tipo == 'Despesa variável'){
+            $('tbody').prepend(`<tr style="background-color: red">
+                <td>R$${addItem.valor}</td>
+                <td>${addItem.tipo}</td>
+                <td>${addItem.desc}</td>
+            </tr>`)
+        } else if (addItem.tipo == 'Despesa fixa'){
+            $('tbody').prepend(`<tr style="background-color: orange">
+                <td>R$${addItem.valor}</td>
+                <td>${addItem.tipo}</td>
+                <td>${addItem.desc}</td>
+            </tr>`)
+        } else if (addItem.tipo == 'Investido'){
+            $('tbody').prepend(`<tr style="background-color: blue">
+                <td>R$${addItem.valor}</td>
+                <td>${addItem.tipo}</td>
+                <td>${addItem.desc}</td>
+            </tr>`)
         }
-        
     })
-
-    if(localStorage.lucro == undefined || localStorage.lucro == 'NaN'){
-        localStorage.lucro = 0;
-    }
-
-    if(localStorage.investimento == undefined || localStorage.investimento == 'NaN'){
-        localStorage.investimento = 0;
-    } 
-
-    function valorInvestido(valor){
-        localStorage.investimento = Number(localStorage.investimento) + valor;
-    }
-
-
-    function valorLucrado(valor,tipo){
-        if(tipo == 'credito'){
-            localStorage.lucro = Number(localStorage.lucro) + valor;
-        } else if(tipo == 'd-fixa' || tipo == "d-variavel"){
-            localStorage.lucro = Number(localStorage.lucro) - valor;
-        }
-
-    }
 })
