1131bb5c7e638632cbb7d5cc15eade6bc532c7ce
Chrystian Coelho <chrystiancoelho3212@outlook.com>
2021-09-18 15:30:59 -0300

    Tentando adicionar o gráfico

--- js/functions.js
+++ js/functions.js
@@ -143,9 +143,78 @@ $(function(){
 
         $('#lucro-span').html('R$'+resultado)
         $('#inv-span').html('R$'+investido)
+        /*****/
+
+        // Adicionando o gráfico no relatório
+        var pontos = [];
+        var dias = [];
+        var meses = [];
+        var valores = [];
+        var soma = 0;
+        var dataSplit;
+
+        for(let n = 0; n < lista.length; n++){
+            dataSplit = (lista[n].data).split('/')
+            pontos.push({
+                dia: dataSplit[0],
+                mes: dataSplit[1]
+            })
+
+            // Colocando os valores nos pontos
+            if(lista[n].tipo == 'Crédito'){
+                soma = soma + Number(lista[n].valor);
+            } else if(lista[n].tipo == 'Despesa fixa' || lista[n].tipo == 'Despesa variável'){
+                soma = soma - Number(lista[n].valor);
+            }
+            valores.push(soma);
+        }
+
+        function ordenarMeses(){
+            for(let m = 0; m < pontos.length; m++){
+                meses.push(pontos[m].mes)
+            }
+
+            return meses.sort()
+        }
+        
+        var ctx = $('#grafico');
+        var grafico = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ordenarMeses(), // Fazer com que organize o array de modo crescente de acordo com os meses e dias
+                datasets: [{
+                    label: 'Grafico',
+                    data: valores,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     }
 
     /*****/
 
-    // Adicionando o gráfico no relatório
+    
 })
